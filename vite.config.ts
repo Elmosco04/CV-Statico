d0f75df1cd99ef80f9d04951b69d95dd415cac94
ElMosco <moschiniale04@gmail.com>
2026-02-16 23:45:37 +0100

    Base: elmosco04.github.io/CV-Statico/

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/CV-Statico/',
+  base: 'elmosco04.github.io/CV-Statico/',
   plugins: [
     vue(),
     vueDevTools(),
