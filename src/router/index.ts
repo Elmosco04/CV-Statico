3328c74995a0c4b5205b62666e2f4d7ea4030590
ElMosco <moschiniale04@gmail.com>
2026-02-18 23:19:50 +0100

    Fix path

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import Esperienze from '@/views/EsperienzeLavorative.vue'
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.url),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
